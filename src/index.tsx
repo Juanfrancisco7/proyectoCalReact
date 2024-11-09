00cc025d2299e8c936a8a3f8280331d23f75634b
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-11-09 11:11:12 +0100

    Actualizacion completa

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("El contenedor root no fue encontrado en el documento.");
+}
+
+const root = createRoot(container);
 root.render(
   <StrictMode>
     <App />
